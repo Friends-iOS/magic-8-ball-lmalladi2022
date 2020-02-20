26111e1c6b6f26443852daf4c914ac8401ea5f8c
leela <leela@email.me>
2020-02-20 10:27:00 -0500

    commit

--- Magic 8 Ball/ViewController.swift
+++ Magic 8 Ball/ViewController.swift
@@ -12,7 +12,14 @@ class ViewController: UIViewController {
     
     let ballArray = [#imageLiteral(resourceName: "ball1.png"),#imageLiteral(resourceName: "ball2.png"),#imageLiteral(resourceName: "ball3.png"),#imageLiteral(resourceName: "ball4.png"),#imageLiteral(resourceName: "ball5.png")]
 
-
-
+    @IBOutlet weak var magic8BallImageViewOne: UIImageView!
+    
+    @IBAction func fortuneButton(_ sender: UIButton) {
+   
+    }
+    
+    
+    
 }
+
 
